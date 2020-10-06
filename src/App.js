9090144e21ac3649a52f90540a1168db6c6414c1
nadudvari <n.peter0131@gmail.com>
2020-10-06 15:23:23 +0200

    responsive

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import image1 from './images/webeste_kep_01.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import { Card, Navbar } from 'react-bootstrap';
+import { Card, Navbar, Container } from 'react-bootstrap';
 
 function App() {
   return (
@@ -12,37 +12,39 @@ function App() {
               <Navbar.Brand href="#home" className="logo">WEBESTE.HU <i style={{fontSize: 12, marginLeft: 10}}>Csak a web és te</i></Navbar.Brand>
           </Navbar>
           <div className="grid">
-              <Card className="Box" style={{ color: "#000", width: 600, backgroundImage: "linearGradient(transparent, #D3D3D3)"}}>
-                  <Card.Body>
-                      <Card.Text>
-                          Ma már egy sikeres vállalkozáshoz elengedhetetlen egy igazán jó weboldal. Ennek segítségével tehetjük magunkat
-                          elérhetővé a világ számára. Termékeinkről, szolgáltatásainkról tájékoztatást nyújtunk jövőbeli megrendelőinek,
-                          így egyszerűbben kapcsolatot létesíthetünk bárkivel.
-                      </Card.Text>
-                  </Card.Body>
-              </Card>
-              <Card className="Box" style={{ color: "#000", width: 800, marginLeft: 550, marginRight: "auto" }}>
-                  <Card.Body>
-                      <Card.Img src={image1} style={{ width: 400, height: "auto", float: "right" }} />
-                      <Card.Text>
-                          A Webeste ebben áll rendelkezésedre! Csak a Web és TE! Segítünk, hogy vállalkozásod a következő szintre emelhesd,
-                          hogy könnyebben elérhessen a világ és hogy sikeresebb légy! Weboldal tervezéssel és készítéssel foglalkozunk,
-                          amit teljesen a te elképzeléseid szerint valósítunk meg, hogy maximálisan azt tükrözze, ami Te és a vállalkozásod
-                          vagytok. Mindezek mellett, már meglévő weboldaladat is fejlesztjük, karbantartjuk, ha csak egy kis felfrissülésre,
-                          változatosságra van szüksége az arculatodnak.
-                      </Card.Text>
-                  </Card.Body>
-              </Card>
-              <Card className="Box" style={{ width: 600 }}>
-                  <Card.Body>
-                      <Card.Text>
-                          Legyen szó kis- vagy nagyvállalatról nálunk megtalálod az elképzelésednek megfelelő oldalt. Személyes blog,
-                          tájékoztató jellegű weboldal vagy esetleg egy nagy webshop, mi segítünk ennek megvalósításában a lehető legjobb
-                          minőségben és áron. Az általunk készített oldalakat kóddal írjuk és személyre szabott árakon dolgozunk, hogy
-                          a legnagyobb elégedettséget tudjuk nyújtani számodra!
-                      </Card.Text>
-                  </Card.Body>
-              </Card>
+              <Container style={{ width: "100%" }}>
+                  <Card className="Box" style={{ color: "#000", width: 600, backgroundImage: "linearGradient(transparent, #D3D3D3)"}}>
+                      <Card.Body>
+                          <Card.Text>
+                              Ma már egy sikeres vállalkozáshoz elengedhetetlen egy igazán jó weboldal. Ennek segítségével tehetjük magunkat
+                              elérhetővé a világ számára. Termékeinkről, szolgáltatásainkról tájékoztatást nyújtunk jövőbeli megrendelőinek,
+                              így egyszerűbben kapcsolatot létesíthetünk bárkivel.
+                          </Card.Text>
+                      </Card.Body>
+                  </Card>
+                  <Card className="Box" style={{ color: "#000", width: 800, float: "right", marginRight: "auto" }}>
+                      <Card.Body>
+                          <Card.Img src={image1} style={{ width: 400, height: "auto", float: "right" }} />
+                          <Card.Text>
+                              A Webeste ebben áll rendelkezésedre! Csak a Web és TE! Segítünk, hogy vállalkozásod a következő szintre emelhesd,
+                              hogy könnyebben elérhessen a világ és hogy sikeresebb légy! Weboldal tervezéssel és készítéssel foglalkozunk,
+                              amit teljesen a te elképzeléseid szerint valósítunk meg, hogy maximálisan azt tükrözze, ami Te és a vállalkozásod
+                              vagytok. Mindezek mellett, már meglévő weboldaladat is fejlesztjük, karbantartjuk, ha csak egy kis felfrissülésre,
+                              változatosságra van szüksége az arculatodnak.
+                          </Card.Text>
+                      </Card.Body>
+                  </Card>
+                  <Card className="Box" style={{ width: 600 }}>
+                      <Card.Body>
+                          <Card.Text>
+                              Legyen szó kis- vagy nagyvállalatról nálunk megtalálod az elképzelésednek megfelelő oldalt. Személyes blog,
+                              tájékoztató jellegű weboldal vagy esetleg egy nagy webshop, mi segítünk ennek megvalósításában a lehető legjobb
+                              minőségben és áron. Az általunk készített oldalakat kóddal írjuk és személyre szabott árakon dolgozunk, hogy
+                              a legnagyobb elégedettséget tudjuk nyújtani számodra!
+                          </Card.Text>
+                      </Card.Body>
+                  </Card>
+              </Container>
           </div>
           <Card style={{ backgroundColor: "#8d8d8d", textAlign: "center" }}>
               <Card.Body>
